a1e9c5986f068d6227a08518549c865543c2b045
Batuhan MERMER <batuhanmrmr@gmail.com>
2021-10-13 19:49:03 +0300

    Tried something

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js">
-
 function encrypt(message = '', key = ''){
     var message = CryptoJS.AES.encrypt(message, key);
     return message.toString();
@@ -10,3 +8,4 @@ function decrypt(message = '', key = ''){
 
     return decryptedMessage;
 }
+console.log();
